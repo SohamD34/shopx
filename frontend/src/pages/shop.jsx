f13e1f6caac93703c1354b606eff8ad68efb1a4a
SohamD34 <deshmukh.2@iitj.ac.in>
2025-07-07 11:59:44 +0530

    assets

---
+++ frontend/src/pages/shop.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from '../components/hero/hero'
+import Popular from '../components/popular/popular'
+import Offers from '../components/offers/offers'
+
+export const Shop = () => {
+  return (
+    <div>
+        <Hero/>
+        <Popular/>
+        <Offers/>
+    </div>
+  )
+}
+
+export default Shop;
