9a4dc46d746dd3c3fe31c5a0b10bcba58bc93022
SohamD34 <deshmukh.2@iitj.ac.in>
2025-07-07 12:00:49 +0530

    added hero components

---
+++ frontend/src/components/hero/hero.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './hero.css'
+import hand_icon from '../assets/Frontend_Assets/hand_icon.png'
+import arrow_icon from '../assets/Frontend_Assets/arrow.png'
+import hero_image from '../assets/Frontend_Assets/hero_image.png'
+
+
+
+export const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className='hero-left'>
+            <h2>NEW ARRIVALS ONLY</h2>
+            <div>
+                <div className="hero-hand-icon">
+                    <p>new</p>
+                    <img src={hand_icon} alt="" />
+                </div>
+                <p>collections</p>
+                <p>for everyone</p>
+            </div>
+
+            <div className="hero-latest-button">
+                <div>Latest Collection</div>
+                <img src={arrow_icon} alt="" />
+            </div>
+        </div>
+
+        <div className='hero-right'>
+            <img src={hero_image} alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Hero;
