7fb1ea40b908e18f79b72ff53b7b533ba858784f
SohamD34 <deshmukh.2@iitj.ac.in>
2025-08-05 19:44:17 +0530

    Added signup, login, items pages

--- frontend/src/pages/loginsignup.jsx
+++ frontend/src/pages/loginsignup.jsx
@@ -2,21 +2,92 @@ import React from 'react'
 import './css/loginsignup.css'
 
 export const LoginSignup = () => {
+
+    const [state, setState] = React.useState("Login");
+    const [formData, setFormData] = React.useState({
+        username: "",
+        email: "",
+        password: ""
+    });
+
+    const changeHandler = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value});
+    }
+
+    const login = async () => {
+        console.log("Login function called", formData);
+        let responseData;
+
+        const res = await fetch('http://localhost:4000/login', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        });
+        console.log('fetched');
+        
+        responseData = await res.json();
+        console.log(responseData);
+
+        if (responseData && responseData.success === true) {
+            localStorage.setItem('auth-token', responseData.authToken);
+            window.location.replace('/');
+        }
+        else{
+            alert(responseData.error);
+        }
+    }
+
+    const signup = async () => {
+        console.log("Signup function called", formData);
+        let responseData;
+
+        const res = await fetch('http://localhost:4000/signup', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        });
+        console.log('fetched');
+        
+        responseData = await res.json();
+        console.log(responseData);
+
+        if (responseData && responseData.success === true) {
+            localStorage.setItem('auth-token', responseData.authToken);
+            window.location.replace('/');
+        }
+        else{
+            alert(responseData.error);
+        }
+    }
+
+
     return (
 
         <div className="loginsignup">
             <div className="loginsignup-container">
-                <h1>Sign Up</h1>
+                <h1>{state}</h1>
 
                 <div className="loginsignup-fields">
-                    <input type="text" className="name" placeholder="Name" />
-                    <input type="email" className="email" placeholder="Email ID" />
-                    <input type="password" className="password" placeholder="Password" />
+
+                    {(state === "Sign Up") 
+                    ? <input name='username' value={formData.username} onChange={changeHandler} type="text" className="name" placeholder="Name" />
+                    :<></>}
+                    <input name='email' value={formData.email} onChange={changeHandler} type="email" className="email" placeholder="Email ID" />
+                    <input name='password' value={formData.password} onChange={changeHandler} type="password" className="password" placeholder="Password" />
+                
                 </div>
 
-                <button className="signup-button">Continue</button>
+                <button className="signup-button" onClick={()=>{state==="Login"?login():signup()}}>Continue</button>
 
-                <p className='loginsignup-login'>Already have an account? <span>Login here</span></p>
+                {(state === "Sign Up")
+                ? <p className='loginsignup-login'>Already have an account? <span onClick={()=>{setState("Login")}}>Login here</span></p>
+                : <p className='loginsignup-login'>Create an account? <span onClick={()=>{setState("Sign Up")}}>Click here</span></p> }
 
                 <div className="loginsignup-agree">
                     <input type="checkbox" name="" id=""/>
